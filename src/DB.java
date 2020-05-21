6960e14a2ffa8f9e946589f9f19292028f458ce9
Emils Baskers <emils.baskers@gmail.com>
2020-05-21 22:02:45 +0300

    add some things

--- src/DB.java
+++ src/DB.java
@@ -1,4 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DB {
-
+	private Connection con;
+	private Statement stat;
+	private PreparedStatement pstat;
+	private ResultSet rs;
+	
+	public DB() {
+		try {
+			this.con = DriverManager.getConnection("jdbc:mysql://localhost:3306/qna_game", "root", "");
+			this.stat = con.createStatement();
+			this.createTables();
+		} catch (SQLException e) {
+			System.out.println("Problems with database setup");
+		}
+	}
+	
+	private String getActiveQuestion() {
+		
+	}
+	
+	private void createTables() {
+		String createQuestionTable = "CREATE TABLE IF NOT EXISTS `tbl_question` (  `id` INT(11) NOT NULL AUTO_INCREMENT,  `question` VARCHAR(500) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',  `answer` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',  `is_answer` TINYINT(1) NOT NULL DEFAULT '0',  `winner_name` VARCHAR(50) NULL COLLATE 'utf8mb4_general_ci',  PRIMARY KEY (`id`) USING BTREE)COLLATE='utf8mb4_general_ci'ENGINE=InnoDB;";
+		String createAnswerTryTable = "CREATE TABLE IF NOT EXISTS `tbl_answer_try` (  `id` INT(11) NOT NULL AUTO_INCREMENT,  `question_id` INT(11) NOT NULL,  `player` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',  `text` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',  `is_correct` TINYINT(1) NOT NULL DEFAULT '0',  `is_winner` TINYINT(1) NOT NULL DEFAULT '0',  `answer_time` DATETIME NOT NULL,  PRIMARY KEY (`id`) USING BTREE)COLLATE='utf8mb4_general_ci'ENGINE=InnoDB;";
+		this.insert(createQuestionTable);
+		this.insert(createAnswerTryTable);
+	}
+	
+	private void insert (String query) {
+		try {
+			this.stat.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println("Problems with insert query ");
+			System.out.println(query);
+		}
+	}
 }
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,12 +1,16 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class GameFrame extends JFrame {
+public class GameFrame extends JFrame implements KeyListener {
+	
 	private int width = 700;
 	private int height = 500;
 	Container cont;
@@ -16,6 +20,8 @@ public class GameFrame extends JFrame {
 	private JLabel tryLabel;
 	private JLabel statusLabel;
 	private JTextField answerInput;
+	private int totalTries;
+	private int playerTries;
 	
 	GameFrame(String question, String answer){
 		this.question = question;
@@ -33,16 +39,54 @@ public class GameFrame extends JFrame {
 		this.cont.setLayout(null);
 		Font titleFont = new Font(Font.SANS_SERIF, Font.BOLD, 20);
 		Font textFont = new Font(Font.SANS_SERIF, Font.PLAIN, 20);
-		this.questionLabel = new JLabel(this.question);
-		this.questionLabel.setForeground(Color.WHITE);
-		this.questionLabel.setFont(titleFont);
-		this.questionLabel.setBounds(60, 30, 500, 30);
-		this.cont.add(this.questionLabel);
+		Font statusFont = new Font(Font.SANS_SERIF, Font.ITALIC, 14);
+		this.questionLabel = createNewLabel(this.question, titleFont, Color.WHITE, 60, 30, 500, 30);
+		this.statusLabel = createNewLabel("Test status", statusFont, Color.WHITE, 60, 100, 500, 30);
 		
-		this.statusLabel = new JLabel("Test status");
-		this.statusLabel.setForeground(Color.WHITE);
-		this.statusLabel.setFont(textFont);
-		this.statusLabel.setBounds(60, 70, 500, 30);
-		this.cont.add(this.statusLabel);
+		this.tryLabel = createNewLabel("", textFont, Color.WHITE, 520, 60, 160, 60);
+		this.tryLabel.setBorder(BorderFactory.createLineBorder(Color.WHITE, 2));
+		this.updateTries();
+		
+		this.answerInput = new JTextField(3);
+		this.answerInput.setBounds(60, 140, 400, 60);
+		this.answerInput.setFont(textFont);
+		this.answerInput.addKeyListener(this);
+		this.cont.add(this.answerInput);
 	}
+	
+	private JLabel createNewLabel(String text, Font font, Color color, int x, int y, int w, int h) {
+		JLabel label = new JLabel(text);
+		label.setForeground(color);
+		label.setFont(font);
+		label.setBounds(x, y, w, h);
+		this.cont.add(label);
+		return label;
+	}
+	
+	private void updateTries() {
+		String tryHtml = "<html><div style='padding: 7px'>Total tries: "+this.totalTries+"<br/>Your tries: "+this.playerTries+"</div></html>";
+		this.tryLabel.setText(tryHtml);
+	}
+
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode()!=KeyEvent.VK_ENTER) return;
+		String text = this.answerInput.getText().trim();
+		if (text.length()==0) return;
+		this.playerTries++;
+		this.totalTries++;
+		this.updateTries();
+		boolean isCorrect = text.equalsIgnoreCase(this.correctAnswer);
+		this.answerInput.setText("");
+		if (isCorrect) {
+			this.statusLabel.setText("Yup, you are right. Congratz!");
+		} else {
+			if (this.playerTries==1) this.statusLabel.setText("Nope. Try again!");
+			this.statusLabel.setText("You have been wrong "+this.playerTries+" times. Try again!");
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
 }
